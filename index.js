5773f3b7805bdab4d05e7e163852169156952b3c
happy-thakur <suryanshsinghstudy@gmail.com>
2018-03-21 01:28:50 +0530

    CORS

--- index.js
+++ index.js
@@ -38,6 +38,28 @@ const mailTransport = nodemailer.createTransport({
 
 const APP_NAME = "Knipnu";
 
+
+// Add headers
+app.use(function(req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+
 app.get("/welcome", (req, res) => {
     sendWelcomeEmail("suryanshsinghstudy@gmail.com", "happy");
     sendEmail("suryanshsinghstudy@gmail.com", "happy", "this is\n done <a href='http://localhost:4200/delete/-L7iNLWr2_9aqGjIxM-7'>cancel</a>", "ha ha ha");
